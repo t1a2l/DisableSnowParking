ac165952fdc28a52ad38e25c79633aff94503243
Tal <talrofeh@gmail.com>
2023-05-27 03:28:55 +0300

    fix from long time ago that has not be uploaded for some reason

--- Code/GenerateSnowPatch.cs
+++ Code/GenerateSnowPatch.cs
@@ -9,13 +9,15 @@ namespace DisableSnowParking
     static class GenerateSnowPatch
     {
         [HarmonyPrefix]
-        public static void Prefix(BuildingInfoBase info, ref Task __result)
+        public static bool Prefix(BuildingInfoBase info, ref Task __result)
 	    {
             var steam_parkings = new [] {"1293869603", "1285201733", "1293869603", "2116510188", "2115188517", "2121900156"};           
             if (info.GetAI() is ParkAI && info.name.Contains("2174913158") || info.GetAI() is BuildingAI && steam_parkings.Any(info.name.Contains))
             {
                 info.m_disableSnow = true;
+                return false;
             }
+            return true;
 	    }
     }
 }
